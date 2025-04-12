c3a912b890e750c48aa7fdecb58d3ab33e91a00d
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2025-04-12 12:36:13 -0500

    Integración de vista usuario a un solo TSX

--- src/pages/ClientesPages/VistaDeEntornoDeSesionUsu.tsx
+++ src/pages/ClientesPages/VistaDeEntornoDeSesionUsu.tsx
@@ -1,7 +1,5 @@
-import { FunctionComponent, useEffect, useState } from "react";
-import BottomFooter from "../../components/BottomFooter";
-import ContenedorFarmaciaYAutoriza from "../../components/ContenedorFarmaciaYAutoriza";
-import ContenedorServicios from "../../components/ContenedorServicios";
+import { FunctionComponent, useEffect, useState, useCallback } from "react";
+import Medicamentos from "../../components/Medicamentos";
 import HeaderTop from "../../components/HeaderTop";
 import styles from "./VistaDeEntornoDeSesionUsu.module.css";
 
@@ -14,12 +12,109 @@ const VistaDeEntornoDeSesinUsu: FunctionComponent = () => {
     setUsername(storedUsername);
   }, []);
 
+  const onMedicamentosContainerClick = useCallback(() => {
+    // Please sync "Vista de entorno de solicitud de citas usuario:cliente" to the project
+  }, []);
+
   return (
     <div className={styles.vistaDeEntornoDeSesinUsu}>
-      <BottomFooter />
+      {/* Contenedor de Opciones Adicionales */}
+      <div className={styles.opcionesAdicionales}>
+        <div className={styles.opcionesAdicionalesChild} />
+        <div className={styles.opcionesAdicionales1}>Opciones adicionales</div>
+        <div className={styles.ctaButton}>
+          <div className={styles.textContainer}>
+            <img
+              className={styles.chatbubblesSharpIcon}
+              alt=""
+              src="/chatbubblessharp.svg"
+            />
+            <div className={styles.cta}>Contactar</div>
+          </div>
+        </div>
+        <div className={styles.ctaButton1}>
+          <div className={styles.textContainer1}>
+            <img
+              className={styles.chatbubblesSharpIcon}
+              alt=""
+              src="/notificationssharp.svg"
+            />
+            <div className={styles.cta1}>Notificaciones</div>
+          </div>
+        </div>
+      </div>
+
       <div className={styles.contenedorServiciosFarmacia}>
-        <ContenedorFarmaciaYAutoriza />
-        <ContenedorServicios />
+        {/* Contenedor de Farmacia y Autorizaciones */}
+        <div className={styles.contenedorFarmaciaYAutoriza}>
+          <div className={styles.contenedorFarmaciaYAutorizaChild} />
+          <div className={styles.reportes}>
+            <div className={styles.repoButton}>
+              <div className={styles.textContainer}>
+                <img
+                  className={styles.mailUnreadIcon}
+                  alt=""
+                  src="/mailunread.svg"
+                />
+                <div className={styles.cta}>
+                  <span>{` `}</span>
+                  <span className={styles.reportes1}>{`Reportes  `}</span>
+                  <span>{` `}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.autorizaciones}>
+            <div className={styles.autoButton}>
+              <div className={styles.textContainer1}>
+                <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+                <div className={styles.cta}>Autorizaciones</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.medicamentos}>
+            <div className={styles.medButton}>
+              <div className={styles.textContainer1}>
+                <img className={styles.medkitIcon} alt="" src="/medkit.svg" />
+                <div className={styles.cta}>Medicamentos</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.ttuloDeFarmaciaYAutorizac}>
+            <div className={styles.farmaciaYAutorizaciones}>
+              Farmacia y autorizaciones
+            </div>
+          </div>
+        </div>
+
+        {/* Contenedor de Servicios */}
+        <div className={styles.contenedorServicios}>
+          <div className={styles.contenedorDeServicios} />
+          <div className={styles.ttuloDeServicios}>
+            <div className={styles.servicios}>Servicios</div>
+          </div>
+          <Medicamentos
+            onMedicamentosContainerClick={onMedicamentosContainerClick}
+            medkit="/medkit1.svg"
+            cTA="Solicitud y cancelación de citas"
+          />
+          <Medicamentos
+            medicamentosLeft="255px"
+            medicamentosTop="72px"
+            medicamentosHeight="100px"
+            medkit="/medkit2.svg"
+            cTA="Solicitud y ayudas diagnósticas"
+            cTAWidth="144px"
+          />
+          <Medicamentos
+            medicamentosLeft="145px"
+            medicamentosTop="192px"
+            medicamentosHeight="69px"
+            medkit="/medkit3.svg"
+            cTA="Historia clínica"
+            cTAWidth="109px"
+          />
+        </div>
       </div>
       <div className={styles.entornoDeBienvenidaParent}>
         <div className={styles.entornoDeBienvenida} />
